39cbce4d9dbc610f8240d8919714f7620d4dc180
xuxy <xuxiaoyu2010@gmail.com>
2023-08-20 14:16:03 +0800

    fix comment

--- bitmap.go
+++ bitmap.go
@@ -1,30 +1,26 @@
 package bitmap
 
-
 import (
 	"fmt"
 )
 
-
-
 type BitMap struct {
 	store []uint64
-	size uint64
+	size  uint64
 }
 
-// new Bitmap
+// new Bitmap to create a bitmap
 // size is the length of total of bit
 // return the pointer of Bitmap
-func New(size uint64) *BitMap{
+func New(size uint64) *BitMap {
 
 	length := size / 64
 
-	if size % 64 > 0 {
+	if size%64 > 0 {
 		length = length + 1
 	}
 
 	return &BitMap{make([]uint64, length), size}
-	
 
 }
 
@@ -33,7 +29,7 @@ func New(size uint64) *BitMap{
 func (b *BitMap) Set(offset uint64) {
 
 	b.checkOffset(offset)
-	index , bit := indexAndOverbit(offset)
+	index, bit := indexAndOverbit(offset)
 	count := uint64(1 << bit)
 
 	b.store[index] = b.store[index] | count
@@ -44,9 +40,9 @@ func (b *BitMap) Set(offset uint64) {
 func (b *BitMap) Clear(offset uint64) {
 
 	b.checkOffset(offset)
-	index , bit := indexAndOverbit(offset)
+	index, bit := indexAndOverbit(offset)
 	count := ^uint64(1 << bit)
-	b.store[index] = b.store[index] & count	
+	b.store[index] = b.store[index] & count
 
 }
 
@@ -55,22 +51,21 @@ func (b *BitMap) Clear(offset uint64) {
 func (b *BitMap) Get(offset uint64) int {
 
 	b.checkOffset(offset)
-	index , bit := indexAndOverbit(offset)
+	index, bit := indexAndOverbit(offset)
 	count := uint64(1 << bit)
 	val := b.store[index] & count
 
 	if val == 0 {
 		return 0
 	}
-	return 1	
+	return 1
 }
 
-func (b *BitMap)checkOffset( offset uint64 ) {
+func (b *BitMap) checkOffset(offset uint64) {
 	if offset > b.size {
 		panic(fmt.Sprintf("BitMap overflow: offset %d bigger than size %d", offset, b.size))
 	}
 }
-
 
 func indexAndOverbit(offset uint64) (index, bit uint64) {
 	return offset / 64, offset % 64
